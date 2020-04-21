79d091014abaeb331c7f6aab5c5bbd5e1d463c88
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2020-04-21 05:02:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ class point {
 
 $(document).ready(function(){
  for(var i=0;i<9;i++){
+ 	if(i%3===0)$('<p> </p>').insertBefore("#grid");
 	for(var d =0;d<9;d++){
-		$(`<input type="text" name="" id="${d}-${i}" placeholder="${d}x${i}">`).insertBefore("#grid");
+		if(d%3===0)$('<spacer type="verticle">   </spacer>').insertBefore("#grid");
+		$(`<input type="text" name="" id="${i}-${d}" placeholder="${i}x${d}">`).insertBefore("#grid");
 	}
 }
 });
@@ -57,6 +59,7 @@ const isFillable = (mat,row,col,num)=>{
     for(let u=0;u<9;u++)if(mat[row][u]===num)return false;
     for(let u=0;u<9;u++)if(mat[u][col]===num)return false;
     if(usedInBox(mat,row-row%3,col-col%3,num)===true)return false;
+    $(`#${row}-${col}`).css('background-color','#70C3E0');
     return true;	
 
 };
@@ -91,3 +94,24 @@ const solve = ()=>{
   	alert('no solution possible');
   }else printSolution(mat);
  };
+/*
+                       3, 0, 6, 5, 0, 8, 4, 0, 0},  
+                      {5, 2, 0, 0, 0, 0, 0, 0, 0},  
+                      {0, 8, 7, 0, 0, 0, 0, 3, 1},  
+                      {0, 0, 3, 0, 1, 0, 0, 8, 0},  
+                      {9, 0, 0, 8, 6, 3, 0, 0, 5},  
+                      {0, 5, 0, 0, 9, 0, 6, 0, 0},  
+                      {1, 3, 0, 0, 0, 0, 2, 5, 0},  
+                      {0, 0, 0, 0, 0, 0, 0, 7, 4},  
+                      {0, 0, 5, 2, 0, 6, 3, 0, 0}};  
+
+3 1 6 5 7 8 4 9 2 
+5 2 9 1 3 4 7 6 8 
+4 8 7 6 2 9 5 3 1 
+2 6 3 4 1 5 9 8 7 
+9 7 4 8 6 3 1 2 5 
+8 5 1 7 9 2 6 4 3 
+1 3 8 9 4 7 2 5 6 
+6 9 2 3 5 1 8 7 4 
+7 4 5 2 8 6 3 1 9 
+*/
